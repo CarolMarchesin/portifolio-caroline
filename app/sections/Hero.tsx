521cdef779560ca9b9d3b6d7c5881b96d2575d22
CarolMarchesin <104083173+CarolMarchesin@users.noreply.github.com>
2025-06-02 21:27:38 -0300

    feat: update Hero section layout and add link to About section

--- app/sections/Hero.tsx
+++ app/sections/Hero.tsx
@@ -1,27 +1,34 @@
 import Image from "next/image";
 import ArrowDown from "../../public/arrow-down.svg";
+import Link from "next/link";
 
 export default function Hero() {
   return (
-    <section className="relative min-h-screen flex flex-col items-center justify-center">
-      <div className="flex flex-col h-screen items-center justify-center">
-        <div className="flex flex-row text-6xl md:text-8xl font-bold gap-2">
+    <section className="flex flex-col h-screen justify-center container mx-auto px-4">
+      <div className="flex flex-col items-center justify-center text-center">
+        <div className="flex flex-row text-5xl md:text-8xl font-bold gap-2">
           <span className="text-gray-800 opacity-80">Caroline</span>
           <span className="text-pink-500">Marchesin</span>
         </div>
 
-        <div className="flex flex-row text-3xl md:text-5xl mt-4">
+        <div className="flex flex-row text-2xl md:text-5xl mt-6">
           <span className="text-gray-800 opacity-70">Front-end Developer</span>
         </div>
 
-        <div className="flex flex-row text-1xl md:text-[16px] gap-6 mt-10">
-          <div className="bg-pink-500 rounded-full px-5 py-4 text-white font-bold cursor-pointer hover:bg-pink-600 transition duration-300">
+        <div className="flex flex-row text-[14px] md:text-[16px] gap-6 mt-14">
+          <Link
+            href="#about"
+            className="bg-pink-500 rounded-full px-3 py-3 md:px-5 md:py-4 text-white font-bold cursor-pointer hover:bg-pink-600 transition duration-300"
+          >
             Conheça meu trabalho
-          </div>
+          </Link>
 
-          <div className="border-2 border-pink-500 rounded-full px-10 py-4 text-gray-800 opacity-70 cursor-pointer hover:text-gray-950 transition duration-300">
+          {/* <Link
+            href="#"
+            className="border-2 border-pink-500 rounded-full px-6 py-3 md:px-10 md:py-4 text-gray-800 opacity-70 cursor-pointer hover:text-gray-950 transition duration-300"
+          >
             Entre em contato
-          </div>
+          </Link> */}
         </div>
       </div>
 
